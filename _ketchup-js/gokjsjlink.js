9bd3290764803381402de66ea10ee169c5d8932b
ccauvang <fakeonthehat@gmail.com>
2025-04-10 21:48:19 +0700

    Fix: Fixed typo.

--- _ketchup-js/gokjsjlink.js
+++ _ketchup-js/gokjsjlink.js
@@ -813,7 +813,7 @@ function sus() {
 sus();
 */
 
-
+/* 
 //funlink.io
 function sus() {
   var randomcode = ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (n => (n ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> n / 4).toString(16)));
@@ -880,8 +880,7 @@ function sus() {
   }, 60 * 1e3);
 };
 sus();
-
-
+*/
 
 /*
 //seotrieuview
