c74d5eb788d4985964cde4914282a979e1f6668e
ccauvang <fakeonthehat@gmail.com>
2025-12-02 20:29:36 +0700

    feat: add some junk code.

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-async function sus(input = 'hi', systemInput = 'Hello Bro', content = '') {
+/* async function sus(input = 'hi', systemInput = 'Hello Bro', content = '') {
     input += 'Respond this in English';
     const dataRaw = {
         "messages": [
@@ -54,5 +54,52 @@ process.stdin.on('data', async (listen) => {
     contentMessage += listen + '\n';
     await sus(`${listen}`, 'Responding like a REAL Bro.', contentMessage);
     // process.exit();
-});
+}); */
 
+
+function findSubs(n1, n2) {
+    if (n1 > n2) return false;
+
+    let freg = {};
+    let lenOfN1 = n1.length;
+    let pointer = 0;
+    for (let sOfN1 = n1.length - 1; sOfN1 > -1; --sOfN1) {
+        if (!freg[n1[sOfN1]]) {
+            freg[n1[sOfN1]] = 1;
+        };
+    }
+
+    for (let sOfN2 = 0; sOfN2 < n2.length; sOfN2++) {
+        if (!freg[n2[sOfN2]]) freg[n2[sOfN2]] = 0;
+
+        const s = freg[n2[sOfN2]];
+        freg[n2[sOfN2]] -= 1;
+        if (s > 0) {
+            lenOfN1 -= 1;
+        } else {
+            while (freg[n2[sOfN2]] < 0) {
+                freg[n2[pointer]] += 1;
+                if (freg[n2[pointer]] > 0) {
+                    lenOfN1 += 1;
+                }
+                pointer++;
+            }
+        }
+
+        if (lenOfN1 == 0) {
+            return true
+        };
+
+        console.log(freg);
+
+    }
+    return false;
+
+}
+
+console.time('lol');
+console.log(findSubs('abc', 'hbbbbbcaaooo'));
+console.timeEnd('lol');
+
+//abc
+//haabcaooo
